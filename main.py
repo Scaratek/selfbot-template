9653b4fdec1b46ffd423e2d6a6bdc5814588e337
Scaratek ( <90155711+Scaratek@users.noreply.github.com>
2023-01-21 23:04:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from discord.ext import commands
+from discord.ext import tasks
+import discord
+import sys
 
+token = sys.argv[1]
+prefix = sys.argv[2]
+
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+
+@bot.event
+async def on_ready():
+    print('-' * 20)
+    print('Logged in as')
+    print(f'User: {bot.user.name}')
+    print(f'ID: {bot.user.id}')
+    print(f'Token: {token}')
+    print('-' * 20)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run(token)
